02e020db3eb58f8f06395c5932bb722e6c084616
Jorge Oliveira <neweracracker@gmail.com>
2014-09-09 17:24:07 +0100

    Reconstructed version 0.6.0
    Reconstructed from:
    https://web.archive.org/web/20190507114644if_/https://gist.githubusercontent.com/NewEraCracker/e545f0dcf64ba816d49b/raw/07f1cb77545435a6af8d6be30d2a42488b7e589c/mod_antiloris.c
    
    Link from:
    https://web.archive.org/web/20190507114611/https://gist.github.com/NewEraCracker/e545f0dcf64ba816d49b
    
    -----BEGIN UPSTREAM CHANGELOG-----
    Add configuration for adjustable limits
    based on the different vectors. Add option
    to ignore local IPs
    -----END UPSTREAM CHANGELOG-----

--- mod_antiloris.c
+++ mod_antiloris.c
@@ -1,5 +1,5 @@
 /*
-   mod_antiloris 0.5.2
+   mod_antiloris 0.6.0
    Copyright (C) 2010 Monshouwer Internet Diensten
 
    Author: Kees Monshouwer
@@ -33,8 +33,8 @@
 #include "scoreboard.h"
 
 #define MODULE_NAME "mod_antiloris"
-#define MODULE_VERSION "0.5.2"
-#define ANTILORIS_DEFAULT_MAX_CONN 20
+#define MODULE_VERSION "0.6.0"
+#define ANTILORIS_DEFAULT_MAX_CONN_PER_TYPE 10
 
 #ifdef APLOG_USE_MODULE
 APLOG_USE_MODULE(antiloris);
@@ -44,16 +44,18 @@ APLOG_USE_MODULE(antiloris);
 #define remote_ip client_ip
 #endif
 
-#ifndef INT_MAX
-#define INT_MAX 32767
-#endif
-
 module AP_MODULE_DECLARE_DATA antiloris_module;
 
 static int server_limit, thread_limit;
 
 typedef struct {
-	signed int limit;
+	/* IP Connection Limits */
+	signed long int read_limit;
+	signed long int write_limit;
+	signed long int other_limit;
+
+	/* Local IP Addresses */
+	apr_array_header_t *local_ips;
 } antiloris_config;
 
 typedef struct {
@@ -61,17 +63,21 @@ typedef struct {
 	int thread_num;
 } sb_handle;
 
-/* Create per-server configuration structure */
+/** Create per-server configuration structure */
 static void *create_config(apr_pool_t *p, server_rec *s)
 {
 	antiloris_config *conf = apr_pcalloc(p, sizeof (*conf));
 
-	conf->limit = ANTILORIS_DEFAULT_MAX_CONN;
+	conf->read_limit  = ANTILORIS_DEFAULT_MAX_CONN_PER_TYPE;
+	conf->write_limit = ANTILORIS_DEFAULT_MAX_CONN_PER_TYPE;
+	conf->other_limit = ANTILORIS_DEFAULT_MAX_CONN_PER_TYPE;
+	conf->local_ips   = apr_array_make(p, 0, sizeof(char *));
+
 	return conf;
 }
 
-/* Parse the IPReadLimit directive */
-static const char *ipreadlimit_config_cmd(cmd_parms *parms, void *mconfig, const char *arg)
+/** Parse IPReadLimit directive */
+static const char *ip_read_limit_config_cmd(cmd_parms *parms, void *mconfig, const char *arg)
 {
 	signed long int limit;
 
@@ -81,22 +87,81 @@ static const char *ipreadlimit_config_cmd(cmd_parms *parms, void *mconfig, const
 	if (!err) {
 		limit = strtol(arg, (char **) NULL, 10);
 
-		if ((limit > INT_MAX) || (limit < 0))
-			return "Integer overflow or invalid number";
+		/* No reasonable person would want more than 2^16. Better would be
+		   to use LONG_MAX but that causes portability problems on win32 */
+		if (limit > 65535) { return "Limit can't be higher than 65535"; }
+		if (limit < 0)     { return "Limit can't be lower than zero"; }
 
-		conf->limit = limit;
+		conf->read_limit = limit;
 	}
 
 	return err;
 }
 
-/* Array describing structure of configuration directives */
+/** Parse IPWriteLimit directive */
+static const char *ip_write_limit_config_cmd(cmd_parms *parms, void *mconfig, const char *arg)
+{
+	signed long int limit;
+
+	antiloris_config *conf = ap_get_module_config(parms->server->module_config, &antiloris_module);
+	const char *err = ap_check_cmd_context (parms, GLOBAL_ONLY);
+
+	if (!err) {
+		limit = strtol(arg, (char **) NULL, 10);
+
+		/* No reasonable person would want more than 2^16. Better would be
+		   to use LONG_MAX but that causes portability problems on win32 */
+		if (limit > 65535) { return "Limit can't be higher than 65535"; }
+		if (limit < 0)     { return "Limit can't be lower than zero"; }
+
+		conf->write_limit = limit;
+	}
+
+	return err;
+}
+
+/** Parse IPOtherLimit directive */
+static const char *ip_other_limit_config_cmd(cmd_parms *parms, void *mconfig, const char *arg)
+{
+	signed long int limit;
+
+	antiloris_config *conf = ap_get_module_config(parms->server->module_config, &antiloris_module);
+	const char *err = ap_check_cmd_context (parms, GLOBAL_ONLY);
+
+	if (!err) {
+		limit = strtol(arg, (char **) NULL, 10);
+
+		/* No reasonable person would want more than 2^16. Better would be
+		   to use LONG_MAX but that causes portability problems on win32 */
+		if (limit > 65535) { return "Limit can't be higher than 65535"; }
+		if (limit < 0)     { return "Limit can't be lower than zero"; }
+
+		conf->other_limit = limit;
+	}
+
+	return err;
+}
+
+/** Parse LocalIPs directive */
+static const char *local_ips_config_cmd(cmd_parms *parms, void *mconfig, const char *arg)
+{
+	antiloris_config *conf = ap_get_module_config(parms->server->module_config, &antiloris_module);
+
+	*(char **) apr_array_push(conf->local_ips) = apr_pstrdup(parms->pool, arg);
+
+	return NULL;
+}
+
+/** Configuration directives */
 static command_rec antiloris_cmds[] = {
-	AP_INIT_TAKE1("IPReadLimit", ipreadlimit_config_cmd, NULL, RSRC_CONF, "Maximum simultaneous connections per IP address"),
+	AP_INIT_TAKE1("IPReadLimit",  ip_read_limit_config_cmd,  NULL, RSRC_CONF, "Maximum simultaneous connections in READ state per IP address"),
+	AP_INIT_TAKE1("IPWriteLimit", ip_write_limit_config_cmd, NULL, RSRC_CONF, "Maximum simultaneous connections in WRITE state per IP address"),
+	AP_INIT_TAKE1("IPOtherLimit", ip_other_limit_config_cmd, NULL, RSRC_CONF, "Maximum simultaneous idle connections per IP address"),
+	AP_INIT_ITERATE("LocalIPs",   local_ips_config_cmd,      NULL, RSRC_CONF, "List of IPs (separated by spaces) whose connection are always allowed"),
 	{NULL}
 };
 
-/* Set up startup-time initialization */
+/** Startup-time initialization */
 static int post_config(apr_pool_t *p, apr_pool_t *plog, apr_pool_t *ptemp, server_rec *s)
 {
 	void *data;
@@ -107,7 +172,7 @@ static int post_config(apr_pool_t *p, apr_pool_t *plog, apr_pool_t *ptemp, serve
 	 * set up our static data on the second call. */
 	apr_pool_userdata_get(&data, userdata_key, s->process->pool);
 	if (!data) {
-		apr_pool_userdata_set((const void *)1, userdata_key,apr_pool_cleanup_null, s->process->pool);
+		apr_pool_userdata_set((const void *)1, userdata_key, apr_pool_cleanup_null, s->process->pool);
 		return OK;
 	}
 
@@ -120,20 +185,23 @@ static int post_config(apr_pool_t *p, apr_pool_t *plog, apr_pool_t *ptemp, serve
 	return OK;
 }
 
+/** Our hook at connection processing */
 static int pre_connection(conn_rec *c)
 {
-	char *remote_ip;
+	/* Remote IP to be used in checking operations */
+	char *remote_ip = c->remote_ip;
 
+	/* loop index variables */
+	int i = 0, j = 0;
+
+	/* running count of number of connections from this address */
+	signed long int ip_read_count = 0, ip_write_count = 0, ip_other_count = 0;
+
+	/* other variables we'll be using */
 	antiloris_config *conf = ap_get_module_config(c->base_server->module_config, &antiloris_module);
 	sb_handle *sbh = c->sbh;
 
-	/* loop index variables */
-	int i, j;
-
-	/* running count of number of connections from this address */
-	int ip_count = 0;
-
-#if AP_MODULE_MAGIC_AT_LEAST(20071023,0)
+#if AP_MODULE_MAGIC_AT_LEAST(20110605,2)
 	/* get the socket descriptor */
 	apr_socket_t *csd = ap_get_conn_socket(c);
 #endif
@@ -141,10 +209,30 @@ static int pre_connection(conn_rec *c)
 	/* scoreboard data structure */
 	worker_score *ws_record;
 
+	/* Save current remote ip in current ws_record */
 	ws_record = &ap_scoreboard_image->servers[sbh->child_num][sbh->thread_num];
-	apr_cpystrn(ws_record->client, c->remote_ip, sizeof(ws_record->client));
+	apr_cpystrn(ws_record->client, remote_ip, sizeof(ws_record->client));
 
-	remote_ip = ws_record->client;
+	/* Take local IPs in consideration */
+	if(conf->local_ips->nelts) {
+		char **ip = (char **) conf->local_ips->elts;
+		for (i = 0; i < conf->local_ips->nelts; i++) {
+#if AP_MODULE_MAGIC_AT_LEAST(20050101,0)
+			ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, c, "Performing IP check versus: \"%s\"", ip[i]);
+#else
+			ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, NULL, "[client %s] Performing IP check versus: \"%s\"", remote_ip, ip[i]);
+#endif
+			if (ap_strcasecmp_match(remote_ip, ip[i]) == 0) {
+#if AP_MODULE_MAGIC_AT_LEAST(20050101,0)
+				ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, c, "Exempted from connection limit");
+#else
+				ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, NULL, "[client %s] Exempted from connection limit", remote_ip);
+#endif
+				/* Pass request to appropriate module */
+				return DECLINED;
+			}
+		}
+	}
 
 	/* Count up the number of connections we are handling right now from this IP address */
 	for (i = 0; i < server_limit; ++i) {
@@ -156,22 +244,36 @@ static int pre_connection(conn_rec *c)
 #endif
 			switch (ws_record->status) {
 				case SERVER_BUSY_READ:
-				case SERVER_BUSY_WRITE:
-				case SERVER_BUSY_KEEPALIVE:
-				case SERVER_BUSY_DNS:
-				case SERVER_BUSY_LOG:
-					if (strcmp(remote_ip, ws_record->client) == 0)
-					ip_count++;
+					/* Handle read state if limit is higher than zero */
+					if (conf->read_limit > 0 && strcmp(remote_ip, ws_record->client) == 0) ip_read_count++;
 					break;
+				case SERVER_BUSY_WRITE:
+					/* Handle write state if limit is higher than zero */
+					if (conf->write_limit > 0 && strcmp(remote_ip, ws_record->client) == 0) ip_write_count++;
+					break;
+				case SERVER_BUSY_KEEPALIVE:
+					/* Handle keep-alive state if limit is higher than zero */
+					if (conf->other_limit > 0 && strcmp(remote_ip, ws_record->client) == 0) ip_other_count++;
+					break;
+				case SERVER_BUSY_LOG:
+				case SERVER_BUSY_DNS:
+				case SERVER_CLOSING:
+				case SERVER_GRACEFUL:
 				default:
+					/* Other states are ignored */
 					break;
 			}
 		}
 	}
 
-	if (ip_count > conf->limit) {
-		ap_log_error(APLOG_MARK, APLOG_WARNING, 0, NULL, "[client %s] Antiloris rejected, too many connections", c->remote_ip);
-#if AP_MODULE_MAGIC_AT_LEAST(20071023,0)
+	/* Deny the request if it exceeds limits */
+	if (ip_read_count > conf->read_limit || ip_write_count > conf->write_limit || ip_other_count > conf->other_limit) {
+#if AP_MODULE_MAGIC_AT_LEAST(20050101,0)
+		ap_log_cerror(APLOG_MARK, APLOG_WARNING, 0, c, "Connection rejected by Antiloris, too many connections");
+#else
+		ap_log_error(APLOG_MARK, APLOG_WARNING, 0, NULL, "[client %s] Connection rejected by Antiloris, too many connections", remote_ip);
+#endif
+#if AP_MODULE_MAGIC_AT_LEAST(20110605,2)
 		apr_socket_close(csd);
 		return DONE;
 #else
@@ -179,15 +281,18 @@ static int pre_connection(conn_rec *c)
 #endif
 	}
 
+	/* Pass request to appropriate module */
 	return DECLINED;
 }
 
+/** Registration of our hooks */
 static void register_hooks(apr_pool_t *p)
 {
 	ap_hook_post_config(post_config, NULL, NULL, APR_HOOK_MIDDLE);
 	ap_hook_process_connection(pre_connection, NULL, NULL, APR_HOOK_FIRST);
 }
 
+/** Our module data */
 module AP_MODULE_DECLARE_DATA antiloris_module = {
 	STANDARD20_MODULE_STUFF,
 	NULL,			/* create per-dir config structures */
